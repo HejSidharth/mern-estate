6929f839a546007ee80f4b9b656d684240713905
Sidharth Hejamadi <sh81@illinois.edu>
2023-11-18 20:19:09 -0600

    made some comments

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Load .env file into process.env
 
-
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO).then(() => {
   console.log('Connected to MongoDB!');
 }).catch(err => {
@@ -12,8 +12,9 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 
-const app = express();
+const app = express(); // Create an Express app
 
+// Add middleware for parsing URL encoded bodies (which are usually sent by browser)
 app.listen(3000, () => {
   console.log('Server iss running on port 3000');
 });
