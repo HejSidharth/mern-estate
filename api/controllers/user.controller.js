a3921cbd0222e38aa6e886034f26b1454c3efa2b
Sidharth Hejamadi <sh81@illinois.edu>
2023-11-24 21:49:25 -0600

    api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+export const test = (req, res) => {
+    res.json({
+      message: "API route is working!",
+    });
+  };
+  // The above code is a simple test route that returns a JSON object with a message property.
