21792008b8bd8b16142d4af71841a2235cfe9874
Sidharth Hejamadi <sh81@illinois.edu>
2023-11-25 20:58:35 -0600

    created middleware and function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
 
 
     const {username, email, password} = req.body; // Get username, email and password from request body
@@ -11,7 +12,7 @@ export const signup = async (req, res) => {
         await newUser.save(); // Save the user
         res.status(201).json("User created successfully!");// Send a 201 response
     } catch (error) {
-        res.status(500).json(error.message);// Send a 500 response if an error occurred
+        next(error); // Pass error to the next middleware
     }
     
 };
