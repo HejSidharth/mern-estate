96889da1f8695943d7880b91a587a0e8ca7eaa0f
Sidharth Hejamadi <sh81@illinois.edu>
2023-11-29 01:52:55 -0600

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import  jwt  from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
 
@@ -16,3 +17,23 @@ export const signup = async (req, res, next) => {
     }
     
 };
+
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body; // Get email and password from request body
+    try {
+        const validUser = await User.findOne({email}); // Find the user with the email
+        if (!validUser) {
+            return next(errorHandler(401, "User not found!")); // If no user is found, send a 401 response
+        }
+        const validPassword = await bcryptjs.compare(password, validUser.password); // Compare the password with the hashed password
+        if (!validPassword) {
+            return next(errorHandler(401, "Invalid email or password!")); // If the password is invalid, send a 401 response
+        }
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET); // Create a token with the user id and a secret key
+        const {password: pass, ...rest} = validUser._doc; // Destructure the user object to get all the properties except password
+        res.cookie("access_token", token, {httpOnly: true}).status(200).json(validUser) // Send the token in a cookie
+    } catch (error) {
+        next(error); // Pass error to the next middleware
+    }
+}
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,33 +1,33 @@
 import React, { useState } from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
+// Component for signing up
 export default function SignUp() {
-  const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+  const [formData, setFormData] = useState({}); // State to store form data
+  const [error, setError] = useState(null); // State to store error message
+  const [loading, setLoading] = useState(false); // State to track loading state
+  const navigate = useNavigate(); // Hook for programmatic navigation
+
+  // Function to handle form input changes
   const handleChange = (e) => {
-    setFormData(
-      {
-        ...formData,
-        [e.target.id]: e.target.value,
-      }
-    );
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
   };
+
+  // Function to handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('/api/auth/signup',
-      {
+      const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        
         body: JSON.stringify(formData),
-      }
-      );
+      });
       const data = await res.json();
       if (data.success === false) {
         setError(data.message);
@@ -41,22 +41,39 @@ export default function SignUp() {
       setLoading(false);
       setError(error.message);
     }
-   
-    console.log(data);
   };
-  
-  console.log(formData)
+
+  console.log(formData); // Log the form data to the console
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="text" placeholder='Username'
-        className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
-        <input type="email" placeholder='Email'
-        className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-        <input type="password" placeholder='Password'
-        className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        <input
+          type="text"
+          placeholder='Username'
+          className='border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder='Email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder='Password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
